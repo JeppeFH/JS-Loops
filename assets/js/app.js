0c6112068d32af93f06b654c88fe1efe02fb3433
JeppeFH <jeppefinch@hotmail.dk>
2024-08-08 13:34:18 +0200

    update js script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,39 @@
 /*Variabler*/
+let gallery = document.querySelector(".gallery");
 let outputForLoop = document.querySelector(".outputForLoop");
 let outputWhileLoop = document.querySelector(".outputWhileLoop");
 let outputDoWhileLoop = document.querySelector(".outputDoWhileLoop");
 
-let cars = ["Suzuki", "Opel", "BMW", "Skoda", "Ford"];
+let imageSource = [
+  "https://picsum.photos/300/300?random=1 ",
+  "https://picsum.photos/300/300?random=2 ",
+  "https://picsum.photos/300/300?random=3 ",
+  "https://picsum.photos/300/300?random=4 ",
+  "https://picsum.photos/300/300?random=5 ",
+  "https://picsum.photos/300/300?random=6 ",
+  "https://picsum.photos/300/300?random=7 ",
+  "https://picsum.photos/300/300?random=8 ",
+];
+
+/*Opgave Gallery loops*/
 
 /*For Loop*/
-for (let i = 0; i < cars.length; i++) {
-  outputForLoop.innerHTML += `<p>${cars[i]}<p>`;
+for (let i = 0; i < imageSource.length; i++) {
+  outputForLoop.innerHTML += `<img src="${imageSource[i]}"  i++">`;
 }
 
 /*While Loop*/
 let i = 0;
 
-while (i < cars.length) {
-  outputWhileLoop.innerHTML += "<p>" + cars[i] + "</p>" i++  
+while (i < imageSource.length) {
+  outputWhileLoop.innerHTML += "<img>" + imageSource[i] + "<img>";
+  i++;
 }
 
 /*Do While Loop*/
 let a = 0;
 
-do { outputWhileLoop.innerHTML += "<p>" + cars[i] + "</p>" a++ 
-}
-
-while (a < cars.length)
+do {
+  outputWhileLoop.innerHTML += "<img>" + imageSource[i] + "<img>";
+  a++;
+} while (a < imageSource.length);
