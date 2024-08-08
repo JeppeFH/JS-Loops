62162900a75c28dd5b17a84b4877ac49e8bf17d7
JeppeFH <jeppefinch@hotmail.dk>
2024-08-08 11:28:52 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,12 @@ let outputForLoop = document.querySelector(".outputForLoop");
 let outputWhileLoop = document.querySelector(".outputWhileLoop");
 let outputDoWhileLoop = document.querySelector(".outputDoWhileLoop");
 
+let cars = ["Suzuki", "Opel", "BMW", "Skoda", "Ford"];
+
 /*For Loop*/
+for (let i = 0; i < cars.length; i++) {
+  outputForLoop.innerHTML += `<p>${cars[i]}<p>`;
+}
 
 /*While Loop*/
 
